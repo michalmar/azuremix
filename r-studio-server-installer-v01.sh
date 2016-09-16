8096e0bc0eb62b968532cb13f4deecd17aea02ad
michalmar <michal.marusan@gmail.com>
2016-09-16 10:29:45 +0200

    Add files via upload

---
+++ r-studio-server-installer-v01.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#download and install
+wget http://mrsactionscripts.blob.core.windows.net/rstudio-server-community-v01/InstallRStudio.sh
+chmod 755 ./InstallRStudio.sh 
+sudo ./InstallRStudio.sh
+
